bf8de45ee87d66eb5c6df9861dac20a4cfca33ea
Hatim Selmun <selmun13@gmail.com>
2024-05-09 10:35:22 +0100

    Old school band order by

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,6 +4,6 @@
 SELECT band_name,
 	CASE
 		WHEN split THEN split - formed
-		ELSE 2020 - formed
+		ELSE 2022 - formed
 	END AS lifespan
 FROM metal_bands WHERE style = "Glam rock" ORDER BY lifespan DESC;
