501c4f60aeb5d14cc134c779e1ce9dab6c67f361
Hatim Selmun <selmun13@gmail.com>
2024-05-14 17:52:42 +0100

    top students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
 """Top students"""
 
-list_all = __import__('8-all').list_all
-
 
 def top_students(mongo_collection):
-    """Takes a db collection
-    Returns students sorted by average score"""
-    students = list_all(mongo_collection)
+    pipeline = [
+            {
+                "$unwind": "$topics"
+            },
+            {
+                "$group": {
+                    "_id": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+            },
+            {
+                "$sort": {"averageScore": -1}
+            },
+            {
+                "$project": {
+                    "_id": 1,
+                    "name": "$_id",
+                    "averageScore": 1
+                }
+            }
+    ]
+    return list(mongo_collection.aggregate(pipeline))
 
-    for student in students:
-        topics = student.get('topics')
-        scores = [topic.get('score') for topic in topics]
-        average_score = sum(scores) / len(scores)
+# list_all = __import__('8-all').list_all
 
-        mongo_collection.update_many(
-                {"name": student.get('name')},
-                {"$set": {"averageScore": average_score}}
-                )
 
-    return mongo_collection.find().sort({"averageScore": -1})
+# def top_students(mongo_collection):
+#     """Takes a db collection
+#     Returns students sorted by average score"""
+#     students = list_all(mongo_collection)
+
+#     for student in students:
+#         topics = student.get('topics')
+#         scores = [topic.get('score') for topic in topics]
+#         average_score = sum(scores) / len(scores)
+
+#         mongo_collection.update_many(
+#                 {"name": student.get('name')},
+#                 {"$set": {"averageScore": average_score}}
+#                 )
+
+#     return mongo_collection.find().sort({"averageScore": -1})
