baec809549a1609f9e576a44f80dbdeebaab997f
Hatim Selmun <selmun13@gmail.com>
2024-05-14 15:02:21 +0100

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     """Takes a db coll the school name and a list of topics
     taught int the school
     Updates the coll passed"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
