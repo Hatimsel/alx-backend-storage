6685b507ff04fde82b2c946ce00d204e2b5b4ce2
Hatim Selmun <selmun13@gmail.com>
2024-05-14 18:23:55 +0100

    fixing 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,6 +6,6 @@ def schools_by_topic(mongo_collection, topic):
     """Takes a db coll, and a topic
     Returns the docs with that topic"""
 
-    result = mongo_collection.find({"topics": "Python"})
+    result = mongo_collection.find({"topics": topic})
 
     return result
