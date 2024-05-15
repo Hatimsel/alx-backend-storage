79cf7aec95f1700f62a7f65c9648e24648f8c126
Hatim Selmun <selmun13@gmail.com>
2024-05-15 23:56:37 +0100

    incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,16 @@ Writing strings to redis
 import redis
 import uuid
 from typing import Union
+from functools import wraps
+
+
+def count_calls(method):
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        method_name = method.__qualname__
+        self._redis.incr(method_name)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -14,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """sets a key/value pair in redis and returns the key"""
         key = str(uuid.uuid1())
@@ -21,6 +32,7 @@ class Cache:
 
         return key
 
+    @count_calls
     def get(self, key, fn=None):
         """Converts the data associated to key
         into the desired data type"""
@@ -31,10 +43,19 @@ class Cache:
             return fn(data)
         return data
 
+    @count_calls
     def get_str(self, key):
         """Converts the data associated with key into str"""
         return self.get(key, lambda d: d.decode("utf-8"))
 
+    @count_calls
     def get_int(self, key):
         """Converts the data associated with key into int"""
         return int(self.get(key))
+
+    def get_count(self, fn):
+        """"""
+        count = self._redis.get(fn)
+        if count:
+            return int(count)
+        return 0
