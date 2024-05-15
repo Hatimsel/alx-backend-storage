30adf8527c39bc02da8d10029d95da76df51537e
Hatim Selmun <selmun13@gmail.com>
2024-05-15 21:07:15 +0100

    convert into desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,8 @@
 Writing strings to redis
 """
 import redis
-from typing import Any
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -14,9 +14,27 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """sets a key/value pair in redis and returns the key"""
         key = str(uuid.uuid1())
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key, fn=None):
+        """Converts the data associated to key
+        into the desired data type"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key):
+        """Converts the data associated with key into str"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key):
+        """Converts the data associated with key into int"""
+        return int(self.get(key))
